bac57fbb2231255d87e68481e8fe4a04f0b5a845
Milen Mihaylov <milen.m.mihailov@gmail.com>
2022-10-10 23:50:42 +0300

    petstagram project

---
+++ petstagram/petstagram/common/urls.py
@@ -0,0 +1,7 @@
+from petstagram.common.views import landing_page, testing_view
+from django.urls import path
+
+urlpatterns = [
+    path('', landing_page, name='index'),
+    path('testing', testing_view, name='testing')
+]
---
+++ petstagram/petstagram/common/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
+
+def testing_view(request):
+    return HttpResponse('It works')
